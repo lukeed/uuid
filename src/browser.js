a63a48ac425883dec7c08db79ff8188d02989f58
Luke Edwards <luke.edwards05@gmail.com>
2020-02-27 23:54:18 -0800

    chore: import `browser` variant

---
+++ src/browser.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var SIZE=4096, HEX=[], IDX=0, BUFFER;
+
+for (var i=0; i < 256; i++) {
+	HEX[i] = (i + 0x100).toString(16).substr(1);
+}
+
+function bounds() {
+	var b = slice(16);
+	b[6] = (b[6] & 0x0f) | 0x40;
+	b[8] = (b[8] & 0x3f) | 0x80;
+	return b;
+}
+
+function slice(size) {
+	if (!BUFFER || ((IDX + size) > SIZE)) {
+		BUFFER = crypto.getRandomValues(new Uint8Array(SIZE));
+		IDX = 0;
+	}
+	return BUFFER.slice(IDX, IDX += size);
+}
+
+export default function () {
+	var i=0, out='', arr=bounds();
+	for (; i < 4; i++) out += HEX[arr[i]];
+	for (out += '-'; i < 6; i++) out += HEX[arr[i]];
+	for (out += '-'; i < 8; i++) out += HEX[arr[i]];
+	for (out += '-'; i < 10; i++) out += HEX[arr[i]];
+	for (out += '-'; i < 16; i++) out += HEX[arr[i]];
+	return out;
+}
