0a7ca547ebf4a2c0aaa7e6d465bf9ffe86769d91
Luke Edwards <luke.edwards05@gmail.com>
2020-02-28 20:52:57 -0800

    chore: add benchmark

---
+++ bench/index.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const { Suite } = require('benchmark');
+const { randomBytes } = require('crypto');
+const isUUID = require('is-uuid');
+
+// https://stackoverflow.com/a/2117523
+function replace_random() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
+
+function replace_crypto() {
+	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, function (c) {
+		return (c ^ randomBytes(1)[0] & 15 >> c / 4).toString(16);
+	});
+}
+
+const contenders = {
+	'String.replace(Math.random)': replace_random,
+	'String.replace(crypto)': replace_crypto,
+	'uuid/v4': require('uuid').v4,
+	'@lukeed/uuid': require('../dist'),
+};
+
+console.log('Validation: ');
+Object.keys(contenders).forEach(name => {
+	try {
+		const lib = contenders[name];
+
+		assert.deepStrictEqual(typeof lib(), 'string', 'returns string');
+		assert.notDeepEqual(lib(), lib(), 'unqiue strings');
+		assert.ok(isUUID.v4(lib()), 'valid UUID.V4');
+
+		console.log('  ✔', name);
+	} catch (err) {
+		console.log('  ✘', name, `(FAILED @ "${err.message}")`);
+	}
+});
+
+
+console.log('\nBenchmark:');
+const bench = new Suite().on('cycle', e => {
+	console.log('  ' + e.target);
+});
+
+Object.keys(contenders).forEach(name => {
+	bench.add(name + ' '.repeat(28 - name.length), () => {
+		contenders[name]();
+	})
+});
+
+bench.run();
