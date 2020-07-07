ce45a2613df5d297775b6443a0369abe13022eb4
Nick Randall <nicksrandall@users.noreply.github.com>
2020-07-07 10:12:42 -0600

    Fix - Support IE 11
    See: https://developer.mozilla.org/en-US/docs/Web/API/Window/crypto

--- src/browser.js
+++ src/browser.js
@@ -6,7 +6,7 @@ for (; IDX < 256; IDX++) {
 
 export default function () {
 	if (!BUFFER || ((IDX + 16) > SIZE)) {
-		BUFFER = crypto.getRandomValues(new Uint8Array(SIZE));
+		BUFFER = (crypto || msCrypto).getRandomValues(new Uint8Array(SIZE));
 		IDX = 0;
 	}
 
